58ba6d044aeb68557811cb0b204daf5acb38ea5a
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-01-06 11:07:45 +0700

    Init base scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.scss'
 import AdminRoute from './routes/admin.route'
 import ClientRoute from './routes/client.route'
-
+import './assets/styles/main.scss';
 function App() {
 
   return (
